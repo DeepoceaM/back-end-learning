c8547e91dc756a354b6399cecdc292ab48121c4a
Tiger <chonlasap.matt@sbac.ac.th>
2026-01-28 15:39:54 +0700

    V2

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+let department = [
+  { id: 1, name: "IT"},
+  { id: 2, name: "DBT"},
+];
+
 app.get("/", (req, res) => {
   res.send("Mr.Apiruk Poolpipat");
+});
+
+app.get("/department", (req, res) =>{
+  res.send(department);
+});
+
+app.post("/department", (req, res) =>{
+  let newDepartment = {
+    id: department.length +1,
+    name: req.body.name,
+  };
+  department.push(newDepartment);
+  res.send(department);
 });
 
 app.listen(port, () => {
